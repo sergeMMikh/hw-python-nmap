ae84759abd5fa31eb805180308f6e23ad00d83a9
МСМ МСМ <s.m.mih@mail.ru>
2026-01-24 21:32:48 +0000

    check for job

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import nmap3
 from pprint import pprint
 
+ip = "192.168.33.46"
+
 if __name__ == '__main__':
     print('Hello, world!')
     # nmap = nmap3.Nmap()
     nmap = nmap3.NmapScanTechniques()
-    pprint(nmap.nmap_version()["compiled_with"])
+    # pprint(nmap.nmap_version()["compiled_with"])
     # results = nmap.scan_top_ports("google.com")
-    results = nmap.scan_top_ports("127.0.0.1")
-    # pprint(results["127.0.0.1"]["ports"])
+    results = nmap.nmap_tcp_scan(ip, args="-sV -sC -p-")
 
-    ports=results["127.0.0.1"]["ports"]
+    ports=results[ip]["ports"]
 
     for port in ports:
         print(port["portid"])
