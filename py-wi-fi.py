3dc9f639c67b3cee0e2d69a03b458fafd79766d2
МСМ МСМ <s.m.mih@mail.ru>
2026-01-25 17:09:46 +0000

    added py-wifi

---
+++ py-wi-fi.py
@@ -0,0 +1,21 @@
+import pywifi
+import time
+from pprint import pprint
+
+wifi = pywifi.PyWiFi()
+iface = wifi.interfaces()[0]
+print(iface.name())
+pprint(iface)
+
+iface.scan()
+time.sleep(2)
+
+result = iface.scan_results()
+
+for net in result:
+    print("-" * 40)
+    print("SSID:   ", net.ssid)
+    print("BSSID:  ", net.bssid)
+    print("Signal: ", net.signal)
+    print("Auth:   ", net.auth)
+    print("Cipher: ", net.cipher)
