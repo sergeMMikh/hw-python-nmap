6d644ff8691957a0198e95bc31603da3a1129535
МСМ МСМ <s.m.mih@mail.ru>
2026-01-28 21:11:47 +0000

    nmap

--- py_nmap.py
+++ py_nmap.py
@@ -1,32 +1,23 @@
 import nmap3
 import os
+import platform
+
 from pprint import pprint
 
+system = platform.system()
 
 ip = "192.168.33.46"
 
+
 def tcp_scan_args():
-    if os.name == "nt":
+    system = platform.system()
+    if system == "Windows":
         return "-sT"
-    else:
+    if system == "Linux" and os.geteuid() == 0:
         return "-sS"
+    return "-sT"
 
-# args = (
-#     # "-sS "                 # SYN scan (полуоткрытый TCP-скан, быстрый и менее шумный)
-#     "-sV "                 # Определение версий сервисов на открытых портах
-#     "-sC "                 # Запуск стандартных NSE-скриптов (default)
-#     "-p- "                 # Сканирование всех TCP-портов (1–65535)
-
-#     "-O "                  # Определение операционной системы (OS fingerprinting)
-#     "--reason "            # Показ причины, почему порт считается open/closed
-#     "--version-all "       # Максимально агрессивное определение версии сервисов
-
-#     "--script-timeout 10m "# Увеличенный таймаут для NSE-скриптов (до 10 минут)
-#     "--max-retries 3 "     # Ограничение количества повторных попыток
-#     "--host-timeout 6h "   # Максимальное время сканирования одного хоста
-
-#     "-T3"                  # Умеренный тайминг (баланс скорости и стабильности)
-# )
+nmap = nmap3.Nmap()
 
 args = (
     f"{tcp_scan_args()} "
@@ -34,12 +25,8 @@ args = (
     "-O "
     "--reason "
     "--version-all "
-    "--script-timeout 10m "
-    "--max-retries 3 "
-    "--host-timeout 6h "
     "-T3"
 )
-
 
 def nmap_it(ip: str):
     print(f'Scanning ip: {ip}')
@@ -48,7 +35,11 @@ def nmap_it(ip: str):
     # pprint(nmap.nmap_version()["compiled_with"])
     # results = nmap.scan_top_ports("google.com")
     # results = nmap.nmap_tcp_scan(ip, args="-sV -sC -p-")
-    results = nmap.nmap_tcp_scan(ip, args=args)
+    # results = nmap.nmap_tcp_scan(ip, args=args)
+    try:
+        results = nmap.nmap_command(args, ip)
+    except Exception as e:
+        print(e)
 
     ports=results[ip]["ports"]
 
